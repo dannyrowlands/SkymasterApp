bcbfa8336c6b13d6603963132c93302dbb845c02
dannyrowlands <dannyrowlands@users.noreply.github.com>
2023-10-17 12:26:00 +0100

    Added pointer to table rows on hover.

--- resources/js/Components/TableRow.jsx
+++ resources/js/Components/TableRow.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const TableRow = ({ id, data, handleClick, modelName }) => {
     return (
-        <tr onClick={() => handleClick(id, modelName)}>
+        <tr onClick={() => handleClick(id, modelName)} className="pointer">
             {data.map((item) => {
                 return <td key={item+Math.floor(Math.random() * 100000)}>{item}</td>;
             })}
