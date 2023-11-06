468a3f2186ca3dc26b739da2ff04773c58469cfe
dannyrowlands <dannyrowlands@users.noreply.github.com>
2023-11-06 12:35:55 +0000

    Refactored functions into separate file.

--- resources/js/Pages/Manifest/View.jsx
+++ resources/js/Pages/Manifest/View.jsx
@@ -5,8 +5,9 @@ import "react-big-calendar/lib/addons/dragAndDrop/styles.css"
 import "react-big-calendar/lib/css/react-big-calendar.css"
 import '../../../css/Calendar.css'
 
-import {DragDropContext, Draggable, Droppable} from 'react-beautiful-dnd';
-import poolReducer from "@/Reducers/PoolReducer.jsx";
+import {DragDropContext, Draggable, Droppable} from 'react-beautiful-dnd'
+import poolReducer from "@/Reducers/PoolReducer.jsx"
+import {doDragEnd, updateJumperManifestDetails} from "@/Utils/Manifest/functions.jsx"
 
 const View = ({ auth, manifests, pool, jumpers, type }) => {
     const preloadedState = {
@@ -51,121 +52,12 @@ const View = ({ auth, manifests, pool, jumpers, type }) => {
     }
 
     useEffect(() => {
-        updateJumperManifestDetails()
+        updateJumperManifestDetails(state)
     }, [state]);
 
     const onDragEnd = useCallback((result) => {
-        doDragEnd(result)
+        doDragEnd(result, state, dispatch)
     }, []);
-
-    async function doDragEnd(result) {
-        if (result.reason === "DROP") {
-            if (!result.destination) {
-                return;
-            }
-
-            if(
-                result.source.droppableId === "jumpersList"
-                && result.destination.droppableId === "poolList"
-            ) {
-                await addToPool(result)
-                    .then (function (response) {
-                        dispatch({type: 'pool/added', payload: result})
-                        reloadJumperList()
-                    }
-                )
-            }
-
-            if(
-                result.source.droppableId === "poolList"
-                && result.destination.droppableId === "jumpersList"
-            ) {
-                await removeFromPool(result)
-                    .then (function (response) {
-                        dispatch({type: 'pool/removed', payload: result})
-                        reloadJumperList()
-                    }
-                )
-            }
-
-            if(
-                result.source.droppableId === "poolList"
-                && result.destination.droppableId === "poolList"
-            ) {
-                dispatch({type: 'pool/moved', payload: result})
-                reloadJumperList()
-            }
-        }
-    }
-
-    async function updateJumperManifestDetails() {
-        return await axios.post(
-            '/manifest/set_details',
-            {
-                poolState : state.pool,
-            }
-        )
-            .catch(function (error) {
-                console.log('ERROR RESPONSE::', error)
-                if(error.code === 'ERR_BAD_REQUEST') {
-                    window.location.replace("/login")
-                }
-            })
-    }
-
-    async function addToPool(result) {
-        return await axios.post(
-            '/pool/add',
-            {
-                id : state.jumpers[result.source.index].id,
-                sequence: result.destination.index,
-                group_identifier: null,
-                note: null
-            }
-        )
-            .then(function (response) {
-                console.log('Saved (bool)::',result)
-            })
-            .catch(function (error) {
-                console.log('ERROR RESPONSE::', error)
-                if(error.code === 'ERR_BAD_REQUEST') {
-                    window.location.replace("/login")
-                }
-            })
-    }
-
-    async function removeFromPool(result) {
-        return await axios.post(
-            '/pool/remove',
-            {
-                id : state.pool[result.source.index].id
-            }
-        )
-            .then(function (response) {
-                console.log('Saved (bool)::',result)
-            })
-            .catch(function (error) {
-                console.log('ERROR RESPONSE::', error)
-                if(error.code === 'ERR_BAD_REQUEST') {
-                    window.location.replace("/login")
-                }
-            })
-    }
-
-    async function  reloadJumperList() {
-        return await axios.get(
-            '/manifest/jumpers'
-        )
-            .then(function (response) {
-                dispatch({ type: 'jumpers/loaded', payload: response })
-            })
-            .catch(function (error) {
-                console.log('ERROR RESPONSE::', error)
-                if(error.code === 'ERR_BAD_REQUEST') {
-                    window.location.replace("/login")
-                }
-            })
-    }
 
     return (
         <AuthenticatedLayout
